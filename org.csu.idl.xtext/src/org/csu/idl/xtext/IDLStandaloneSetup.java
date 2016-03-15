ca0f1f28c5433abee895155814f0477fe36fa8a9
KeSUN <ke.sun.x@gmail.com>
2016-03-15 17:41:20 +0100

    Fix the error of unresolved proxy http://idlmm/1.0 by overriding the register method to register the IdlmmPackage in StandaloneSetup.

--- org.csu.idl.xtext/src/org/csu/idl/xtext/IDLStandaloneSetup.java
+++ org.csu.idl.xtext/src/org/csu/idl/xtext/IDLStandaloneSetup.java
@@ -1,6 +1,11 @@
 
 package org.csu.idl.xtext;
 
+import org.csu.idl.idlmm.IdlmmPackage;
+import org.eclipse.emf.ecore.EPackage;
+
+import com.google.inject.Injector;
+
 /**
  * Initialization support for running Xtext languages 
  * without equinox extension registry
@@ -10,5 +15,13 @@ public class IDLStandaloneSetup extends IDLStandaloneSetupGenerated{
 	public static void doSetup() {
 		new IDLStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
+	
+	@Override
+    public void register(Injector injector) {
+        if (!EPackage.Registry.INSTANCE.containsKey("http://idlmm/1.0")) {
+            EPackage.Registry.INSTANCE.put("http://idlmm/1.0", IdlmmPackage.eINSTANCE);
+        }
+        super.register(injector);
+    }
 }
 
